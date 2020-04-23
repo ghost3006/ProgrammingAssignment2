ce35c1f1aeb2034c53c1db4811f9cdfdf0723a61
ghost30 <prateekjha30@gmail.com>
2020-04-23 11:41:18 +0530

    commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  # makes a cache matrix from a given matrix
+  # assign the value NULL for the first initialization
+  
+  cacheMatrix <- NULL
+  
+  # define the function named 'setMatrix'
+  
+  setMatrix <- function(y) {
+    x <<- y
+    cacheMatrix <<- NULL
+  }
+  
+  # define the method named 'getMatrix'
+  
+  getMatrix <- function() x
+  
+  # define the method named 'setCache'
+  
+  setCache <- function(inverse) cacheMatrix <<- inverse
+  
+  # define the method named 'getCache'
+  # return the cached inverse of 'x'
+  
+  getCache <- function() cacheMatrix
+  
+  
+  
+  list(setMatrix = setMatrix,
+       getMatrix = getMatrix,
+       setCache = setCache,
+       getCache = getCache)
+  
+  
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  
+  # return the inverse of a given matrix utilizing the cache
+  
+  cacheMatrix <- x$getCache()
+  
+  # if the content is not null then: return the result 
+  
+  if (!is.null(cacheMatrix)) {
+    message("loading cache matrix...")
+    return(cacheMatrix)
+  }
+  
+  # if the content is empty: 
+  else {
+    dMatrix <- x$getMatrix()
+    cacheMatrix <- solve(dMatrix, ...)
+    x$setCache(cacheMatrix)
+    return(cacheMatrix)
+  }
 }
